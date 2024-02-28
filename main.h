a4db28649c4df0a60bb8510f937ed9db24ea5c9e
marangawanjugu <marangawanjugu@gmail.com>
2024-02-29 01:13:31 +0300

    updating header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#endif
